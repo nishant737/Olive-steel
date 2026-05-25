ef4b95d25fe7d9bec5d52cd86c4ea96d3882a964
Nishanth shetty <nishanthshetty@Nishanths-MacBook-Air.local>
2026-05-25 13:14:05 +0530

    fixed final ui issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng:  { optimizationLevel: 7 },
+      mozjpeg:  { quality: 72 },
+      pngquant: { quality: [0.65, 0.80], speed: 4 },
+      svgo:     { plugins: [{ name: 'removeViewBox' }] },
+      webp:     { quality: 72 },
+    }),
+  ],
 })
